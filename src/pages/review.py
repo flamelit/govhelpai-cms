2b6aa410c8a20007b967dca4704b9fa377aa1c36
tom-flamelit <tom@flamelit.tech>
2023-12-09 09:08:43 -0500

    WIP update to remove certain elements

--- src/pages/review.py
+++ src/pages/review.py
@@ -42,6 +42,33 @@ st.markdown(
     [data-testid="collapsedControl"] {
         display: none
     }
+    div[data-testid="stToolbar"] {
+    visibility: hidden;
+    height: 0%;
+    position: fixed;
+    }
+    div[data-testid="stDecoration"] {
+    visibility: hidden;
+    height: 0%;
+    position: fixed;
+    }
+    div[data-testid="stStatusWidget"] {
+    visibility: hidden;
+    height: 0%;
+    position: fixed;
+    }
+    #MainMenu {
+    visibility: hidden;
+    height: 0%;
+    }
+    header {
+    visibility: hidden;
+    height: 0%;
+    }
+    footer {
+    visibility: hidden;
+    height: 0%;
+    }
 </style>
 """,
     unsafe_allow_html=True,
